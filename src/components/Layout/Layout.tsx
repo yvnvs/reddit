fcb7dc581075776188614e241bb3e70763244325
Yunus <yunusinne@gmail.com>
2023-03-10 22:42:16 +0300

    navbar

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/Navbar";
+
+type LayoutProps = {
+  children: any;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import Layout from "@/components/Layout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from "../chakra/theme";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
