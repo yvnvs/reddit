02ff129ead4f68ba548a8f48aca5342f3bc0b8a7
Yunus <yunusinne@gmail.com>
2023-03-10 22:41:52 +0300

    chakra theme settings

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,13 +1,10 @@
-// 1. Import `extendTheme`
 import { extendTheme } from "@chakra-ui/react";
-import "@fontsource/open-sans/300.css";
-import "@fontsource/open-sans/400.css";
-import "@fontsource/open-sans/700.css";
+import { Button } from "./button";
 
 export const theme = extendTheme({
   colors: {
     brand: {
-      100: "#FF3c00",
+      100: "#FF3C00",
     },
   },
   fonts: {
@@ -15,13 +12,12 @@ export const theme = extendTheme({
   },
   styles: {
     global: () => ({
-        body:{
-            bg:"gray.200";
-        },
+      body: {
+        bg: "gray.200",
+      },
     }),
   },
-  components:{
-    // Button
-    
-  }
+  components: {
+    Button,
+  },
 });
